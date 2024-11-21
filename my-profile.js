780e6243c0a6f9825aeb0b89e2f6ab7657116049
demirose41 <demirose41@gmail.com>
2024-11-20 22:34:51 -0800

    completed part 1

--- my-profile.js
+++ my-profile.js
@@ -1,7 +1,30 @@
 window.onload = () => {
-    console.log("onload")
+    
+    // Part 1A
+
+    // Create <h1 id="name"> of my name
     const h1 = document.createElement("h1")
     h1.innerText = "Demi"
     h1.setAttribute('id', "name")
     document.body.appendChild(h1)
+
+    // create UL
+    const aboutMe = ["I think I am", "I am I am", "I glad I am", "I know I am"]
+    const ul = createUl(aboutMe)
+
+    
+    // append ul to body
+    document.body.appendChild(ul)
+}
+
+// part 1B
+function createUl(array){
+    const ul = document.createElement("ul");
+    // create li
+    for ( const ele of array ){
+        let newLi = document.createElement("li")
+        newLi.innerText = ele
+        ul.appendChild(newLi)
+    }
+    return ul
 }
